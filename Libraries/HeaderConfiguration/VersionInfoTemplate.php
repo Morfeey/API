b9633a767323ae74837365b26458951ee7d2505d
Morfeey <moorfeey@gmail.com>
2018-07-17 14:23:57 +0300

    rebase

---
+++ Libraries/HeaderConfiguration/VersionInfoTemplate.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Burkovnikov
+ * Date: 017 17.07.18
+ * Time: 11:54
+ */
+
+namespace API\Libraries\HeaderConfiguration;
+
+
+abstract class VersionInfoTemplate
+{
+    public $Version;
+    public $Readiness;
+}
---
+++ REST/Versions/Two/VersionInfo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Burkovnikov
+ * Date: 017 17.07.18
+ * Time: 12:03
+ */
+
+namespace API\REST\Versions\Two;
+
+
+class VersionInfo
+{
+
+}
